140d6db934a3404e9cf2a2a131a34834390108bb
Fedi Haddadi <86842075+Fedi-Haddadi@users.noreply.github.com>
2024-12-25 13:08:47 +0100

    Update settings.py

--- universalCom/settings.py
+++ universalCom/settings.py
@@ -138,7 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'authentication.unicom@gmail.com'
-EMAIL_HOST_PASSWORD = 'zdpd qfdj ikho vwlu'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
